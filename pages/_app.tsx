5a9f10c91e742c5f922341f0318f149833518ea8
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-08-25 01:25:59 +0900

    アクセシビリティ対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,12 @@ import "../styles/color.css";
 import "../styles/font.css";
 
 import type { AppProps } from "next/app";
-import Head from "next/head";
 
 import { Layout } from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Layout>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Component {...pageProps} />
     </Layout>
   );
---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
