238f43dda1109ad744d951748feef78e0c11d1fb
Shuhei Akutagawa <readonly@akutagawa.dev>
2021-08-24 23:18:31 +0900

    余白調整 (#21)

--- components/SocialAccountLinks.tsx
+++ components/SocialAccountLinks.tsx
@@ -1,38 +1,30 @@
 import { css } from "@emotion/react";
-import { VoidFunctionComponent } from "react";
+import { HTMLAttributes, VoidFunctionComponent } from "react";
 
-import { Screen } from "../styles/media";
 import { size } from "../styles/size";
 import { SocialAccountLinkGitHub } from "./SocialAccountLinkGitHub";
 import { SocialAccountLinkSpotify } from "./SocialAccountLinkSpotify";
 import { SocialAccountLinkTwitter } from "./SocialAccountLinkTwitter";
 import { SocialAccountLinkZenn } from "./SocialAccountLinkZenn";
 
-export const SocialAccountLinks: VoidFunctionComponent = () => {
-  return (
-    <nav css={styles.container}>
-      <SocialAccountLinkTwitter />
-      <SocialAccountLinkZenn />
-      <SocialAccountLinkGitHub />
-      <SocialAccountLinkSpotify />
-    </nav>
-  );
-};
+export type SocialAccountLinksProps = HTMLAttributes<HTMLElement>;
+
+export const SocialAccountLinks: VoidFunctionComponent<SocialAccountLinksProps> =
+  ({ ...props }) => {
+    return (
+      <nav css={styles.container} {...props}>
+        <SocialAccountLinkTwitter />
+        <SocialAccountLinkZenn />
+        <SocialAccountLinkGitHub />
+        <SocialAccountLinkSpotify />
+      </nav>
+    );
+  };
 
 const styles = {
-  container: [
-    css`
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-around;
-
-      padding: ${size(0)} ${size(8)};
-    `,
-    Screen.md(css`
-      padding: ${size(4)} ${size(12)};
-    `),
-    Screen.lg(css`
-      padding: ${size(8)} ${size(24)};
-    `),
-  ],
+  container: css`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  `,
 };
