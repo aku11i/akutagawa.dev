ea5ed1031e68db431dc9452c28570f7c4e30d6f0
Shuhei Akutagawa <shuhei.akutagawa@uzabase.com>
2021-11-25 17:00:49 +0900

    publish: Next.js のデータフェッチについて

--- articles.ts
+++ articles.ts
@@ -6,4 +6,10 @@ export type Article = {
 
 export const RSS_LIST: string[] = ["https://zenn.dev/aktriver/feed"];
 
-export const OTHER_ARTICLES: Article[] = [];
+export const OTHER_ARTICLES: Article[] = [
+  {
+    title: "Next.js のデータフェッチについて",
+    url: "https://speakerdeck.com/aku11i/next-dot-js-falsedetahuetutinituite",
+    publishedDate: "2021-11-24",
+  },
+];
