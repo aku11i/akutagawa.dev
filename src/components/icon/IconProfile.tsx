7640aaa6385b4d130274d5c25ef6c7ab568833f3
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2022-01-02 02:11:40 +0900

    fix: Optimize profile image size

--- src/components/icon/IconProfile.tsx
+++ src/components/icon/IconProfile.tsx
@@ -4,17 +4,20 @@ import { VoidFunctionComponent } from "react";
 import profile from "../../../public/profile.jpg";
 
 export type ProfileIconProps = {
-  //
+  size: string | number;
 };
 
-export const IconProfile: VoidFunctionComponent<ProfileIconProps> = () => {
+export const IconProfile: VoidFunctionComponent<ProfileIconProps> = ({
+  size,
+}) => {
   return (
     <div className="relative w-full h-full">
       <Image
         src={profile}
         alt="profile icon"
         data-kimochii-pointer="lighter"
-        layout="fill"
+        width={size}
+        height={size}
         className="rounded-full"
       />
     </div>
