fc5827111b132b4325633cbc1c2a0b2d8c54a0ae
Shuhei Akutagawa <readonly@akutagawa.dev>
2022-01-09 01:26:16 +0900

    refactor: Make _document.tsx function component (#159)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,22 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
+import { VoidFunctionComponent } from "react";
 
-class MyDocument extends Document {
-  render(): JSX.Element {
-    return (
-      <Html lang="ja">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="alternate"
-            type="application/rss+xml"
-            href="/articles.rss"
-            title="aku11i's RSS feed"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const MyDocument: VoidFunctionComponent = () => (
+  <Html lang="ja">
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        href="/articles.rss"
+        title="aku11i's RSS feed"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
 export default MyDocument;
