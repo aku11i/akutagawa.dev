a0002aac18eaa065c2320172fbba456c76190f23
Shuhei Akutagawa <shuhei.akutagawa@uzabase.com>
2022-01-08 21:41:17 +0900

    feat: Add skeleton screen for articles

--- src/components/display/Articles.tsx
+++ src/components/display/Articles.tsx
@@ -3,36 +3,53 @@ import { useMemo, VoidFunctionComponent } from "react";
 
 import { Article } from "../../types/article";
 import { ExternalLink } from "../general/ExternalLink";
+import { Skeleton } from "../general/Skeleton";
 import { Typography } from "../general/Typography";
 
 type ArticleItemProps = {
-  article: Article;
+  article: Article | undefined;
 };
 
 const ArticleItem: VoidFunctionComponent<ArticleItemProps> = ({ article }) => {
   const date = useMemo(
-    () => format(new Date(article.publishedDate), "yyyy-MM-dd"),
-    [article.publishedDate]
+    () => article && format(new Date(article.publishedDate), "yyyy-MM-dd"),
+    [article]
   );
 
-  const hostname = useMemo(() => new URL(article.url).hostname, [article.url]);
+  const hostname = useMemo(
+    () => article && new URL(article.url).hostname,
+    [article]
+  );
 
   return (
-    <article>
-      <ExternalLink href={article.url} block>
-        <Typography as="h3" className="text-lg">
-          {article.title}
-        </Typography>
-        <Typography className="text-sm">
-          {date} {hostname}
-        </Typography>
-      </ExternalLink>
-    </article>
+    <li className="py-4">
+      {article ? (
+        <article>
+          <ExternalLink href={article.url} block>
+            <Typography as="h3" className="text-lg">
+              {article.title}
+            </Typography>
+            <Typography className="text-sm">
+              {date} {hostname}
+            </Typography>
+          </ExternalLink>
+        </article>
+      ) : (
+        <div>
+          <div className="py-px h-7">
+            <Skeleton />
+          </div>
+          <div className="py-px my-[2px] w-4/12 h-5">
+            <Skeleton />
+          </div>
+        </div>
+      )}
+    </li>
   );
 };
 
 export type ArticlesProps = {
-  articles: Article[];
+  articles: Article[] | undefined;
 };
 
 export const Articles: VoidFunctionComponent<ArticlesProps> = ({
@@ -43,12 +60,13 @@ export const Articles: VoidFunctionComponent<ArticlesProps> = ({
       <Typography as="h2" className="text-2xl">
         Articles
       </Typography>
+
       <ul className="m-4 list-none">
-        {articles.map((_) => (
-          <li key={_.url} className="py-4">
-            <ArticleItem article={_} />
-          </li>
-        ))}
+        {articles
+          ? articles.map((_) => <ArticleItem key={_.url} article={_} />)
+          : Array(5)
+              .fill(null)
+              .map((_, i) => <ArticleItem key={i} article={undefined} />)}
       </ul>
     </section>
   );
---
+++ src/components/general/Skeleton.tsx
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+import { VoidFunctionComponent } from "react";
+
+export type SkeletonProps = {
+  className?: string;
+};
+
+export const Skeleton: VoidFunctionComponent<SkeletonProps> = ({
+  className,
+}) => {
+  return (
+    <div
+      className={classNames(
+        "w-full h-full bg-gray-400 rounded-md opacity-50",
+        className
+      )}
+    />
+  );
+};
