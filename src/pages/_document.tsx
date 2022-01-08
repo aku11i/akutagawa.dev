15cef820237977173e2eb46d7d7ca6ace8b3e498
Shuhei Akutagawa <readonly@akutagawa.dev>
2022-01-08 22:12:22 +0900

    chore: Add feed link to html (#155)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,12 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            href="/articles.rss"
+            title="aku11i's RSS feed"
+          />
         </Head>
         <body>
           <Main />
