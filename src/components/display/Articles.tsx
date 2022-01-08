91ee687596bb301bb2d95ea68fafd55958781401
Shuhei Akutagawa <shuhei.akutagawa@uzabase.com>
2022-01-09 02:14:08 +0900

    feat: Add RSS feed link

--- src/components/display/Articles.tsx
+++ src/components/display/Articles.tsx
@@ -1,6 +1,9 @@
 import { VoidFunctionComponent } from "react";
 
 import { Article } from "../../types/article";
+import { ExternalLink } from "../general/ExternalLink";
+import { Typography } from "../general/Typography";
+import { IconRss } from "../icon/IconRss";
 import { ArticleList, ArticleListSkeleton } from "./ArticleList";
 
 export type ArticlesProps = {
@@ -10,9 +13,29 @@ export type ArticlesProps = {
 export const Articles: VoidFunctionComponent<ArticlesProps> = ({
   articles,
 }) => {
-  if (!articles) {
-    return <ArticleListSkeleton />;
-  }
+  return (
+    <section>
+      <div className="flex items-center">
+        <Typography as="h2" className="inline-block text-2xl">
+          Articles
+        </Typography>
 
-  return <ArticleList articles={articles} />;
+        <ExternalLink
+          openInNewTab
+          className="inline-block mb-1 ml-3 w-5 h-5"
+          href="https://akutagawa.dev/articles.rss"
+        >
+          <IconRss />
+        </ExternalLink>
+      </div>
+
+      <div className="mt-4">
+        {articles ? (
+          <ArticleList articles={articles} />
+        ) : (
+          <ArticleListSkeleton />
+        )}
+      </div>
+    </section>
+  );
 };
