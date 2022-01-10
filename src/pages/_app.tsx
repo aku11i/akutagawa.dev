85a2936a8babd2e7586073ed2bd4a5208af14257
Shuhei Akutagawa <shuhei.akutagawa@uzabase.com>
2022-01-10 19:04:30 +0900

    refactor: KimochiiPointerの処理をhooksに分ける

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,13 @@ import "../styles/global.css";
 import "../styles/color.css";
 import "../styles/font.css";
 
-import { KimochiiPointer } from "kimochii-pointer";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 
 import { DefaultLayout } from "../components/layout/DefaultLayout";
+import { useKimochiiPointer } from "../hooks/KimochiiPointer";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  useEffect(() => {
-    if (!process.browser) return;
-    const pointer = new KimochiiPointer();
-    pointer.mount();
-
-    return () => {
-      pointer.unmount();
-    };
-  }, []);
+  useKimochiiPointer();
 
   return (
     <>
